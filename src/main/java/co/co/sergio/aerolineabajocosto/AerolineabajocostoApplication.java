76b2a63a6fef2a70aca2194791a468477b1555d2
sergio rodriguez <sarv9208@hotmail.com>
2021-12-07 10:18:23 -0500

    Creación de backend entrega

---
+++ src/main/java/co/co/sergio/aerolineabajocosto/AerolineabajocostoApplication.java
@@ -0,0 +1,13 @@
+package co.co.sergio.aerolineabajocosto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AerolineabajocostoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AerolineabajocostoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/co/sergio/aerolineabajocosto/AerolineabajocostoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.co.sergio.aerolineabajocosto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AerolineabajocostoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
